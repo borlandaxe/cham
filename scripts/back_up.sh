b041f457e757c631f3b94c183c19b9fc35642b8f
zhangheng <my@email.com>
2017-03-28 08:29:10 +0800

    mod

--- scripts/back_up.sh
+++ scripts/back_up.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-alias hp="http_proxy=http://127.0.0.1:8123"
-alias hsp="https_proxy=http://127.0.0.1:8123"
-
-sync_flags="-u"
-
-dirs="repos cham"
-
 rsync -ur --exclude '.git/**' ~/develop/cham/ ~/develop/github/cham && \
 	cd ~/develop/github/cham && \
 	git add * && git commit -a -m "mod" && git push
 
-:<<C
+alias hp="http_proxy=http://127.0.0.1:8123"
+alias hsp="https_proxy=http://127.0.0.1:8123"
+
+sync_flags="-u"
+dirs="repos cham"
+
 for dir in $dirs
 do
 	hp rclone sync $sync_flags --exclude '.git/**' ~/develop/$dir/ gdrive:/$dir
 done
+
+:<<C
 C
