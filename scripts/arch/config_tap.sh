b8ddc51aa5af973dde7bb5ef0020017ac982c7fe
zhangheng <my@email.com>
2017-03-31 15:00:37 +0800

    mod

--- scripts/arch/config_tap.sh
+++ scripts/arch/config_tap.sh
@@ -1,3 +1,6 @@
+
+outlet=$1
+
 sudo ip link add name br0 type bridge &&
 sudo ip addr add 192.168.100.1/24 dev br0 &&
 sudo ip link set br0 up &&
@@ -8,4 +11,4 @@ sudo ip link set tap0 master br0 &&
 sudo ip link set tap0 up &&
 
 sudo sysctl net.ipv4.ip_forward=1 &&
-sudo iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o wlp4s0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o $outlet -j MASQUERADE
